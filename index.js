4f4c3bc356967d274444a646f105573e0d2eb113
nluuarch@gmail.com <nluuarch@gmail.com>
2021-11-07 17:58:12 -0800

    commit tree 925a455757f20bbb8128540150f100dffdd52ea5

--- index.js
+++ index.js
@@ -1,1 +1,51 @@
 // Code your solution here
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+
+// function findMatching(drivers, value) {
+//     for (const name of drivers) {
+//         if (name === value);
+//         console.log(name);
+//     }
+//   }
+
+// findMatching(drivers, "Bobby");
+
+
+// how does filterTest relate to input name(?)
+
+
+function findMatching(someArray, name){ 
+    function findBobbies(driver){
+         if (driver.toLowerCase() === name.toLowerCase()) {
+            return true;
+         }
+    }
+    let allBobbies = someArray.filter(findBobbies);
+    return allBobbies;
+}
+
+
+function fuzzyMatch(someArray, nameLetters){
+    function findMatchingLetterName(driver){
+      if (driver.startsWith(nameLetters)){
+        return true;
+      } 
+    }
+    let namesWithStartLetters = someArray.filter(findMatchingLetterName);
+    return namesWithStartLetters;
+  }
+  
+
+  function matchName(someObjects, name) {
+    function findNamesTowns(driver){
+      if (name === driver.name){
+        return true;
+      }
+    } 
+    let bobbiesTowns = someObjects.filter(findNamesTowns);
+    return bobbiesTowns;
+  }
+
+
+
